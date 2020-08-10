d6868c640825812c47ee6e5d6f08f9cd59f1a14e
ritesh-raw <riteshraawat@gmail.com>
2020-08-10 17:12:56 +0530

    first commit

---
+++ WenAppNoDockerFile/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WenAppNoDockerFile.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
